e30380ebc9b020e61078a9a982ac6b5cf8f1c7c9
Nishitiyer <nishitiyer@example.com>
2026-03-14 22:02:28 +0530

    auto: update changes at 2026-03-14 22:02:27

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('reactflow')) return 'vendor-reactflow';
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
